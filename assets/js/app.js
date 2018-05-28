44309a9d7741873d4d118ac0de8b6f0c7cb5c7fa
Violeta <morada.libertad@gmail.com>
2018-05-27 20:48:32 -0400

    JS listo pero no funcional

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,10 +7,17 @@ function discover(btnFind) {
         alert("Tienes un pequeñ@ Toddler");
         mensaje.innerHTML = "Your child is advancing from infancy toward and into the preschool years. During this time, his physical growth and motor development will slow, but you can expect to see some tremendous intellectual, social, and emotional changes.";
     } else if (3 < age < 5) {
-        alert("Tienes un pequeñ@ Preschool");
-        mensaje.innerHTML = "Your child is advancing from infancy toward and into the preschool years. During this time, his physical growth and motor development will slow, but you can expect to see some tremendous intellectual, social, and emotional changes.";
+          alert("Tienes un pequeñ@ Preschool");
+          mensaje.innerHTML = "Your child is advancing from infancy toward and into the preschool years. During this time, his physical growth and motor development will slow, but you can expect to see some tremendous intellectual, social, and emotional changes.";
     } else if (5 < age < 12) {
-        alert("Tienes un mini Gradeschooler");
-        mensaje.innerHTML = "Your child should feel confident in her ability to meet the challenges in her life. This sense of personal power evolves from having successful life experiences in solving problems independently, being creative and getting results for her efforts.";
+          alert("Tienes un mini Gradeschooler");
+          mensaje.innerHTML = "Your child should feel confident in her ability to meet the challenges in her life. This sense of personal power evolves from having successful life experiences in solving problems independently, being creative and getting results for her efforts.";
+    } else if (12 < age < 18) {
+           alert("Tienes un Teen salvaje");
+         mensaje.innerHTML = "Adolescence can be a challenge for parents. Your youngster may at times be a source of frustration and exasperation, not to mention financial stress. But these years also bring many, many moments of joy, pride, laughter and closeness.";
+    } else {
+            alert("Ya es un Young Adult, let him go");
+            mensaje.innerHTML = "A young adult who goes away to a college or a job far from home has to build a social support system from the ground up. At the same time, he may have to acclimate himself to a drastically different environment.";
     };
+
 };
